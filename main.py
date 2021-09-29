ce12d0dc84c54fe4a4f89ec498eb561ea2b06dc1
Punit Kumar <punitzen@gmail.com>
2021-09-29 11:15:55 +0530

    added args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
+import argparse
+import os, sys
 import time
 import datetime
 from datetime import timezone
@@ -6,17 +7,17 @@ from datetime import timezone
 FREQ = 0.1
 CYCLETIME = 1/FREQ
 
-def run_network_analyser():
+def run_network_analyser(file_name):
     print(" [+] Running Network Analyser")
     
     os.system(
-        "tshark -T fields -e frame.time -e data.data -w sniffed_network.pcap -F pcap -c 3000")
+        "tshark -T fields -e frame.time -e data.data -w {} -F pcap -c 3000".format(file_name))
 
     time.sleep(3)
 
-    os.system("python network_analyzer.py --pcap sniffed_network.pcap --packet 1 --data_frame src,dst,sport,dport --stats num,graph --suspicion true --payload get,post")
+    os.system("python network_analyzer.py --pcap {} --packet 1 --data_frame src,dst,sport,dport --stats num,graph --suspicion true --payload get,post".format(file_name))
 
-def main():
+def main(file_name):
     t0 = time.perf_counter()
     time_counter = t0
     while 1:
@@ -29,9 +30,19 @@ def main():
 
         milliseconds_since_epoch = datetime.datetime.now(timezone.utc)
         print('\n',milliseconds_since_epoch)
-        run_network_analyser()
+        run_network_analyser(file_name)
         time_counter += CYCLETIME
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Network Analyzer')
+    parser.add_argument('--pcap', metavar='<pcap file name>',
+                        help='pcap file to parse', required=True)
+    args = parser.parse_args()
+
+    file_name = args.pcap
+    if not os.path.isfile(file_name):
+        print('"{}" does not exist'.format(file_name), file=sys.stderr)
+        sys.exit(0)
+
+    main(file_name)
